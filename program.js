46aa654108d311bf4c7b00024e2022f2081a46d0
Mr-Titos <arthur.titos@gmail.com>
2019-10-01 17:49:49 +0200

    add a common command : add birthday

--- program.js
+++ program.js
@@ -41,19 +41,33 @@ try {
 bot.on('message', msg => {
 if(msg.author.bot == false ) {
     if(msg.content.substring(0,1) == prefix) {
-        if(msg.content.substring(1,14) == "birthday add ") { //Faire en sorte que cette commande soit usable que des ADMINISTRATEURS
-            var validiteBirthday = true;                //Puis en faire une 2em ou on peut enregister son propre anniv
-            addBirthday(msg).catch(function(err) { msg.reply(err); validiteBirthday = false; }).then(function() {
-                if(validiteBirthday == true) {
-                    msg.reply("L'anniversaire a bien été ajouté !");
-                    loadBirthday();
-                    validiteBirthday = true;
-                } else {
-                    validiteBirthday = true;
-                }
-            });
+        if(msg.content.substring(1,14) == "birthday add ") { 
+            if(msg.member.hasPermission("ADMINISTRATOR")) {
+                var validiteBirthday = true;
+                addBirthdayAdmin(msg).catch(function(err) { msg.reply(err); validiteBirthday = false; }).then(function() {
+                    if(validiteBirthday == true) {
+                        msg.reply("L'anniversaire a bien été ajouté !");
+                        loadBirthday();
+                        validiteBirthday = true;
+                    } else {
+                        validiteBirthday = true;
+                    }
+                });
+            } else 
+                msg.reply("Vous devez être admin pour utiliser cette commande !");
         } else if(msg.content.substring(1, 5) == "help") {
-            msg.reply("3 commandes sont disponibles : \r\n- " + prefix + "birthday add @mention Jour Mois Annee \r\n- " + prefix + "prefix <Nouveau Prefix> \r\n- " + prefix + "help")
+            msg.reply("3 commandes sont disponibles : \r\n- " + prefix + "birthday add @mention Jour Mois Annee (Seulement pour les admins) \r\n- " + prefix + "add birthday Jour Mois Annee (Enregistre juste son anniversaire) \r\n- " + prefix + "prefix <Nouveau Prefix> \r\n- " + prefix + "help")
+        } else if(msg.content.substring(1,14) == "add birthday ") {
+            var validiteBirthday2 = true;
+                addBirthday(msg).catch(function(err) { msg.reply(err); validiteBirthday2 = false; }).then(function() {
+                    if(validiteBirthday2 == true) {
+                        msg.reply("Votre anniversaire a bien été ajouté !");
+                        loadBirthday();
+                        validiteBirthday2 = true;
+                    } else {
+                        validiteBirthday2 = true;
+                    }
+                });
         }
         else if(msg.content.substring(1,7) == "prefix") {
             loadPrefix(msg.content.substring(8,9));
@@ -87,11 +101,47 @@ function timerBirthday() {
 } catch(exc) { console.log(exc); }
 }
 
-function addBirthday(msg) {
+function addBirthdayAdmin(msg) {
     // command frame :<prefix>birthday add <@131930102224125954> day month year
     return new Promise(function (resolve, reject1) {
     try{
-        processBirthday(msg).then(function(id) {
+        processBirthdayAdmin(msg).then(function(id) {
+        var day = dayBirthday *1;
+        var month = monthBirthday *1;
+        var year = yearBirthday;
+        var label;
+        var idTemp;
+        var name;
+        var userTemp = bot.fetchUser(id);
+        userTemp.then(function(data) {
+            label = data.username;
+            idTemp = msg.guild.id;
+            name = data.id;
+            console.log("/birthday add " + name + " " + day + "-" + month + "-" + year + "-" + idTemp);
+            verifBirthday(idTemp, name).then(function() {
+                var jsonData = '{"id":"' + idTemp + '"' + ',"name":"' + name + '"' +',"day":"' + day +'"' + ',"month":"' + month +'"' +',"year":"' + year +'"' + ',"label":"' + label +'"' +',"etat":"false"}';
+                var student = JSON.parse(jsonData);
+                tabBirthhday.Birthday.push(student);
+                stringifyBirthday = JSON.stringify(tabBirthhday, null, 2);
+                fs.writeFile('Birthday.json', stringifyBirthday, (err) => {
+                    if (err) throw err;
+                    console.log('Birthday written to file');
+                    resolve();
+                });
+            }).catch(function(err) { reject1("Impossible d'enregister cet anniversaire car cette personne est déjà enregistré sur ce serveur !"); console.log(err); });
+        });
+        }).catch(function() { reject1("Une erreur a été détéctée dans les données, veuillez la régler."); })
+        
+    } catch(err) { reject("Une erreur technique s'est produite lors de l'ajout de l'anniversaire, veuillez contactez #Titos#2671"); console.log(err); }
+    });
+        
+}
+
+function addBirthday(msg) {
+    // command frame :<prefix>add birthday day month year
+    return new Promise(function (resolve, reject1) {
+    try{
+        processBirthday(msg, msg.author.id).then(function(id) {
         var day = dayBirthday *1;
         var month = monthBirthday *1;
         var year = yearBirthday;
@@ -139,7 +189,7 @@ function loadBirthday() {
     });
 }
 
-function processBirthday(msg) {
+function processBirthdayAdmin(msg) {
     return new Promise(function (resolve, reject) {
         var splitmsg = msg.content.split(' ');
         var finalId = new Array();
@@ -156,6 +206,23 @@ function processBirthday(msg) {
             reject();
         } else {
             console.log("Finalmsg-" + finalId + "---" + splitmsg[2].trim() + "-" + splitmsg[3].trim() + "-" + splitmsg[4].trim());
+            resolve(finalId);
+        }
+    });
+}
+
+function processBirthday(msg, idfinal) {
+    return new Promise(function (resolve, reject) {
+        var splitmsg = msg.content.split(' ');
+        var finalId = idfinal;
+        dayBirthday = splitmsg[2].trim();
+        monthBirthday = splitmsg[3].trim();
+        yearBirthday = splitmsg[4].trim();
+        
+        if(!(dayBirthday *1 <32 && dayBirthday*1 > 0) || !(monthBirthday*1 > 0 && monthBirthday*1 < 13) || !(yearBirthday*1 > 1950 && yearBirthday*1 < 2019)) {
+            reject();
+        } else {
+            console.log("Finalmsg-" + finalId + "---" + "-" + splitmsg[3].trim() + "-" + splitmsg[4].trim());
             resolve(finalId);
         }
     });
